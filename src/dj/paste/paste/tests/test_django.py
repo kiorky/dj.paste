cd97d438d9def73354a880018b4f37c61a422f16
kiorky user <kiorky@judith.(none)>
2009-04-15 16:20:20 +0200

    doc

--- src/dj/paste/paste/tests/test_django.py
+++ src/dj/paste/paste/tests/test_django.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (C)2007 'AFPy'
-
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
